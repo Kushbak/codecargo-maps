cce79164cce1bd48e5b09fcd0590b70296aee616
Kushbak Mamytov <mamytovkushbak@gmail.com>
2023-07-10 20:07:54 +0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import TrimbleMaps from "@trimblemaps/trimblemaps-js";
+import MapMenus from "@trimblemaps/trimblemaps-mapmenus";
 
 function App() {
+  useEffect(() => {
+    const API_KEY = process.env.REACT_APP_TRIMBLE_MAPS;
+    if (!API_KEY) return;
+
+    TrimbleMaps.APIKey = API_KEY;
+    const map = new TrimbleMaps.Map({
+      container: "map",
+      style: TrimbleMaps.Common.Style.TRANSPORTATION,
+      center: new TrimbleMaps.LngLat(-74.566234, 40.49944),
+      zoom: 8,
+    });
+    const myRoute = new TrimbleMaps.Route({
+      routeId: "myRoute1s",
+      showStops: true,
+      stops: [
+        new TrimbleMaps.LngLat(-74.629749, 40.26118),
+        new TrimbleMaps.LngLat(-72.566234, 40.89944),
+        new TrimbleMaps.LngLat(-74.566234, 40.49944),
+        new TrimbleMaps.LngLat(-74.566234, 40.33944),
+      ],
+    });
+    map.on("load", function () {
+      myRoute.addTo(map);
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="map"></div>
     </div>
   );
 }
